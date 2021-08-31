7efac7829120d0bab30c907337f7c022071bdaac
Ana Cecchi <anacecchi99@gmail.com>
2021-08-30 22:04:33 -0300

    atividade 2

---
+++ cliente.py
@@ -0,0 +1,10 @@
+class Cliente:
+
+    def __init__(self, Nome, CPF):
+        self.nome = Nome
+        self.CPF = CPF
+
+input("Digite seu nome:")
+input("Digite seu CPF")
+
+print("Bem-vindo" + Nome")
---
+++ teste.py
@@ -0,0 +1,9 @@
+from cliente import Cliente
+
+
+
+v_nome = " "
+print(v_nome)
+
+v_cpf = " "
+print(v_cpf)
