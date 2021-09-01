d1d5e607e970adf3c26cbb8ae4b8ec96ef1f95cc
Ana Cecchi <anacecchi99@gmail.com>
2021-09-01 19:31:08 -0300

    renomeia pasta 02 para subir arquivos

---
+++ 02_agoravai/conta_corrente.py
@@ -0,0 +1,7 @@
+class Conta_Corrente
+
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
---
+++ 02_agoravai/conta_poupança.py
@@ -0,0 +1,6 @@
+class Conta_Poupança
+
+    def __init__(self, numero, titular, saldo,):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
