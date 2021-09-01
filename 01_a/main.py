afc9d314b3cff391107795ff0b916b1e73693da4
Ana Cecchi <anacecchi99@gmail.com>
2021-09-01 18:32:02 -0300

    Exercícios

---
+++ 01_a/main.py
@@ -0,0 +1,12 @@
+from unidade_federativa import UnidadeFederativa
+
+print("Unidades Federativas Brasileiras")
+
+unidades_federativas = UnidadeFederativa.get_lista_uf()
+
+for counter, value in enumerate(unidades_federativas):
+    print(counter, "-", value)
+
+sigla = input('Digite a sigla do estado')
+uf = UnidadeFederativa.get_UF(sigla)
+print("A UF da sigla " + sigla + " é " + uf)
--- teste.py
+++
@@ -1,9 +0,0 @@
-from cliente import Cliente
-
-
-
-v_nome = " "
-print(v_nome)
-
-v_cpf = " "
-print(v_cpf)
