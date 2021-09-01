dcd8b56d3dd89737584ad38684e75140faea8c37
Ana Cecchi <anacecchi99@gmail.com>
2021-09-01 19:27:33 -0300

    add 02 copia

---
+++ 02_copia/main.py
@@ -0,0 +1,11 @@
+class Main:
+
+    def __init__(self):
+        self.contabancaria = []
+        self.conta_corrente = []
+        self.conta_poupança = []
+
+    def cadastrar_conta_corrente(self):
+
+
+
